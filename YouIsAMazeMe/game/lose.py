ab51c6b8d88069498e75b2837d36a016ecca9d25
Jake Ard <ard20001@byui.edu>
2021-10-18 12:25:07 -0600

    quit button on lose screen

--- YouIsAMazeMe/game/lose.py
+++ YouIsAMazeMe/game/lose.py
@@ -14,10 +14,13 @@ class Lose(arcade.View):
         arcade.start_render
         self.texture.draw_sized(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
         arcade.draw_text('Retry', constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2 - 200, arcade.color.CAMEL, font_size = 40, anchor_x='center')
-        # arcade.draw_rectangle_outline(constants.SCREEN_WIDTH / 2 + 1, constants.SCREEN_HEIGHT / 2 - 185, 135, 57, arcade.color.WHITE)
+        arcade.draw_text('Quit', constants.SCREEN_WIDTH / 2 - 10, constants.SCREEN_HEIGHT / 2 - 270, arcade.color.CAMEL, font_size = 40, anchor_x='center')
+        # arcade.draw_rectangle_outline(constants.SCREEN_WIDTH / 2 - 8, constants.SCREEN_HEIGHT / 2 - 251, 107, 49, arcade.color.WHITE)
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         if _x in range(266, 537) and _y in range(58, 173):
             view = game.director.MainWindow()
             view.setup()
             self.window.show_view(view)
+        elif _x in range(285, 500) and _y in range(49, 148):
+            arcade.exit()
