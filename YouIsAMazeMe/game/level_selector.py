cd99eb6c9db72857ce7bd1849aed82ad584543e0
Jake Ard <ard20001@byui.edu>
2021-11-06 21:18:57 -0600

    fixed level selector with bigger screen

--- YouIsAMazeMe/game/level_selector.py
+++ YouIsAMazeMe/game/level_selector.py
@@ -13,19 +13,19 @@ class LevelSelector(arcade.View):
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.KHAKI)
-        arcade.draw_text('Levels', int(constants.SCREEN_WIDTH / 2 * constants.WIDTH_RATIO), int(550 * constants.HEIGHT_RATIO), arcade.color.RUST, font_size=35, anchor_x='center')
+        arcade.draw_text('Levels', int(constants.SCREEN_WIDTH / 2 + 1 * constants.WIDTH_RATIO), int(550 * constants.HEIGHT_RATIO), arcade.color.RUST, font_size=35, anchor_x='center')
         arcade.draw_text('Back', int(60 * constants.WIDTH_RATIO), int(25 * constants.HEIGHT_RATIO), arcade.color.RUST, font_size=25, anchor_x='center')
         level = 0
-        y = 400
+        y = 400 * constants.HEIGHT_RATIO
         while level != self.num_files:
             for i in range(int(200 * constants.WIDTH_RATIO), int(600 * constants.WIDTH_RATIO) + 1, int(200 * constants.WIDTH_RATIO)):
                 level += 1
                 if level % 4 == 0:
-                    y -= 150
+                    y -= 150 * constants.HEIGHT_RATIO
                 if level < 10:
-                    arcade.draw_text(f'0{level}', i, y, arcade.color.RUST, font_size=35, anchor_x='center')
+                    arcade.draw_text(f'0{level}', i, int(y), arcade.color.RUST, font_size=35, anchor_x='center')
                 else:
-                    arcade.draw_text(f'{level}', i, y, arcade.color.RUST, font_size=35, anchor_x='center')
+                    arcade.draw_text(f'{level}', i, int(y), arcade.color.RUST, font_size=35, anchor_x='center')
                 self.press_ranges.append((int(i - 25 * constants.WIDTH_RATIO), int(i + 25 * constants.WIDTH_RATIO), int(y + 35 * constants.HEIGHT_RATIO), int(y - 1 * constants.HEIGHT_RATIO), level))
                 if level == self.num_files:
                     break
